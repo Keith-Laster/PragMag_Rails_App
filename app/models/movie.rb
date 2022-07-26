10b30e31de6b38d1bc1134045b39187a26d298c7
Keith Laster <lasterk9@gmail.com>
2022-07-25 19:08:43 -0500

    querried db for movies already released and added this model method to the index controller.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ApplicationRecord
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
+
+  def self.released
+    where('released_on < ?', Time.now).order('released_on desc')
+  end
 end
