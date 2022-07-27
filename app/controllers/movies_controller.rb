82bb1b73d095e118fe9863bb01793fd39e6c33f4
Keith Laster <lasterk9@gmail.com>
2022-07-27 13:23:03 -0500

    added flash messages for create, update, and destroy actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,7 +14,7 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params[:id])
     if @movie.update(movie_params)
-      redirect_to @movie
+      redirect_to @movie, notice: 'Movie successfully updated!'
     else
       render :edit
     end
@@ -27,7 +27,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
     if @movie.save
-      redirect_to @movie
+      redirect_to @movie, notice: 'Movie successfully created!'
     else
       render :new
     end
@@ -36,7 +36,7 @@ class MoviesController < ApplicationController
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to movies_url
+    redirect_to movies_url, alert: 'Movie was successfully delted!'
   end
 
   private
