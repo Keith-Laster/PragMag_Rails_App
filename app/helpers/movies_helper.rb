c4b79b15608900207f898bfdcbead6f8d3820ce9
Keith Laster <lasterk9@gmail.com>
2022-07-29 16:47:00 -0500

    added model logic to display a movie's avg. stars.

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -17,4 +17,13 @@ module MoviesHelper
     m.released_on.nil? ? 'TBD' : m.released_on.strftime('%Y')
     # ternary^
   end
+
+  def average_stars(movie)
+    avg_stars = movie.average_stars
+    if avg_stars == 0.0
+      content_tag(:strong, 'No reveiews')
+    else
+      pluralize(number_with_precision(avg_stars, precision: 1), 'star')
+    end
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -18,4 +18,8 @@ class Movie < ApplicationRecord
   def self.released
     where('released_on < ?', Time.now).order('released_on desc')
   end
+
+  def average_stars
+    reviews.average(:stars) || 0.0
+  end
 end
