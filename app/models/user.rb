a0c6bcf50281bdba3c3e3092567b9ce1579e22c1
Keith Laster <lasterk9@gmail.com>
2022-07-30 11:35:44 -0500

    created user account model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true,
+                    format: { with: /\S+@\S+/ },
+                    uniqueness: { case_sensative: false }
+end
