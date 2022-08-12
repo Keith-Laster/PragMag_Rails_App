3ccfd37d26a59be916cefeccdf3f1743d45a7869
Keith Laster <lasterk9@gmail.com>
2022-08-11 22:58:01 -0500

    new through relationship: movies have many fans

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :fans, through: :favorites, source: :user
 
   validates :title, :released_on, :duration, presence: true
   validates :description, length: { minimum: 25 }
